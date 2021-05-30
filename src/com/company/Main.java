d146a81aeb88dda417974649d11deedd9d61894f
Abrams <dima.abramchik@gmail.com>
2021-05-30 04:11:04 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int i = 0;
         int array[] = {10, 5, 6, 8, 9, 7, 1, 5, 8, 2};
+        findEvenElements(array);
+    }
+
+    public static void findEvenElements(int[] array) {
         System.out.print("Элементы массива,стоящие на четных позициях: ");
-        for (; i < array.length; i += 2) {
+        for (int i = 0; i < array.length; i += 2) {
             System.out.print(array[i] + "  ");
         }
     }
